e996c52592213d07437d33018c5d66a8ffec2a1e
LucasGoulart24 <Goulart7720@gmail.com>
2024-10-24 19:22:50 -0300

    projeto_atualizado

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,24 @@
 import './App.css';
-import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
-      <nav className='menu'>
-        <h3>Logo</h3>
+      <nav className='Menu'>
+        <h3>M.P Studio</h3>
         <ul>
-          <li><a>Home</a></li>
-          <li><a>Sobre</a></li>
-          <li><a>Serviço</a></li>
-          <li><a>Contato</a></li>
+          <li><a href='#'>Home</a></li>
+          <li><a href='#'>Sobre</a></li>
+          <li><a href='#'>Serviço</a></li>
+          <li><a href='#'>Contato</a></li>
         </ul>
       </nav>
-      <div className='principal'>
-        <div className='card'>
-          <h1>Hello World</h1>
-          <p>Esse é meu primeiro site em ReactJS, se você não sabe o que é ReactJS, clique no botão para saber mais.</p>
-          <button><a href='https://react.dev/'>Saiba Mais</a></button>
-        </div>
-        <div className='logo'>
-          <img src={logo} alt='logo'></img>
-        </div>
+      <div className='Principal'>
+        <h1>M.P Studio</h1>
+        <p>A Music Player Studio é um site para você baixar seus gêneros musicais favoritos</p>
+      </div>
+      <div className='Pesquisa'>
+        <input type='texto' placeholder='Digite o gênero musical'></input>
+        <button>Pesquisar</button>
       </div>
     </div>
   );
